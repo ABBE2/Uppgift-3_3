25e5047aad4803f3c2e0ddf8731361ed38d29509
Admin <Admin@TEB127030>
2023-02-10 09:04:44 +0100

    Add project files.

---
+++ Uppgift 3_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace uppgift3_3
+{
+    class uppgift3_3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int tid = Convert.ToInt32(Console.ReadLine());
+            int pris = (tid * 80);
+            if (pris > 950)
+                Console.WriteLine("det blir 950kr");
+            else
+            {
+                Console.WriteLine("Det blir " + pris + "kr");
+            }
+        }
+    }
+}
